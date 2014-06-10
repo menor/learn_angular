00d190957b354b2a6af77447f4e75223ff7b49d2
menor <josemenor@gmail.com>
2014-06-10 09:58:56 +0200

    Some more work on lesson 6

---
+++ 06_directives/c/app.js
@@ -0,0 +1,18 @@
+var app = angular.module( "twitterApp", []);
+
+app.controller( 'AppCtrl', function( $scope ){
+  $scope.loadMoreTweets = function() {
+    alert( " Loading Tweets" );
+  };
+  $scope.deleteTweets = function(){
+    alert( "deleting tweets!");
+  }
+});
+
+app.directive( "enter", function(){
+  return function( scope, element, attrs ){
+    element.bind( "mouseenter", function(){
+      scope.$apply( attrs.enter );
+    });
+  };
+});
